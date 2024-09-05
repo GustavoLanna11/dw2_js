56104ddad59587b4441ba4399ccef9ae493bf3e0
Gustavo Lanna <99662123+GustavoLanna11@users.noreply.github.com>
2024-09-05 19:36:04 -0300

    Add files via upload

--- nodejs_express/aula.02-view-engine-ejs/index.js
+++ nodejs_express/aula.02-view-engine-ejs/index.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
 //rota de vídeos
 app.get("/videos/:videos?", (req,res) =>{
   const listaVideos = ['Video1', 'Video2', 'Video3', 'Video4']
-  const videos = req.params.nome
+  const videos = req.params.videos
   res.render("videos", {videos:videos, listaVideos:listaVideos})
 })
 
@@ -42,18 +42,29 @@ app.get("/perfil/:nome?", (req, res) => {
 })
 
 
-
-
-
-
-
+//rota pedidos
+app.get("/pedidos", (req,res) => {
+  //array de objetos com os pedidos
+  
+  const pedidos = [
+    {produto: "Celular", valor: 3000}, 
+    {produto: "Computador", valor: 4000}, 
+    {produto: "Tablet", valor: 2000}, 
+    {produto: "Notebook", valor: 3800}]
+  res.render("pedidos", {
+    //enviando o array de objetos para a página
+    pedidos: pedidos
+  })
+})
 
 //Iniciando o servidor na porta 8080
-app.listen(8080, (error) => {
+
+const port = 8080
+app.listen(port, (error) => {
   if (error) {
     console.log(`Ocorreu um erro: ${error}`);
   } else {
-    console.log("Servidor online!");
+    console.log(`Servidor online em: http://localhost:${port}`);
   }
 });
 
